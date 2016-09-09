97b08f99cd80e32d4599ad8f24a614e61b306a6b
tsarachan <tes353@nyu.edu>
2016-09-09 17:44:22 -0400

    TurnManager becomes Player
    Changed architecture; TurnManager is now a script for each individual player.

---
+++ WarmUp/Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour {
+
+	public float timeDuration = 1.0f;
+	float timer = 0.0f;
+
+	List<int> initiative = new List<int>();
+
+	bool canAttack = false;
+
+	public KeyCode strong;
+	public KeyCode fast;
+	public KeyCode tricky;
+
+	void Update()
+	{
+		timer += Time.deltaTime;
+
+		if (timer >= timeDuration)
+		{
+			ResolveNextInitiative();
+		}
+
+		if (canAttack) { ListenForInput(); }
+	}
+
+	//Take the next action in each player's initiative list
+	//if it's 1 (for "go"), the player gets to act
+	//if it's 2 (for "wait"), the player can't go yet.
+	void ResolveNextInitiative()
+	{
+		if (initiative.Count > 0)
+		{
+			if (initiative[0] == 1) { canAttack = true; }
+
+			initiative.Remove(0);
+		}
+	}
+
+	void ListenForInput()
+	{
+		if (Input.GetKeyDown(strong)) { }
+	}
+}
