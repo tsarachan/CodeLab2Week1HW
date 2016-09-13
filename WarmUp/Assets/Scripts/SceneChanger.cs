971b59109c39dc50a4b612829782a8b5000c46ff
tsarachan <tes353@nyu.edu>
2016-09-13 15:58:06 -0400

    Implemented title scene
    Added simple title scene. Moved quit and restart functionality so that it loads with the title scene, and instructions continue throughout.

---
+++ WarmUp/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneChanger : MonoBehaviour {
+
+	const string GAME_SCENE = "EmptyScene";
+	const string TITLE_SCENE = "Title scene";
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Return))
+		{
+			if (SceneManager.GetActiveScene().name == TITLE_SCENE) { SceneManager.LoadScene(GAME_SCENE); }
+		}
+	}
+}
